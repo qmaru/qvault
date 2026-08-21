1350e929a115ff606ce88c190d0489c63764be24
unknown <csmcc2@hotmail.com>
2026-08-21 16:09:00 +0800

    direct create

--- dbs/dbs.go
+++ dbs/dbs.go
@@ -21,17 +21,8 @@ func CreateIndexes() error {
 	db := GetDB()
 
 	return db.Transaction(func(tx sqlitep.Tx) error {
-		_, err := db.ExecWithTx(tx, "DROP INDEX IF EXISTS idx_user_api_key_hash;")
-		if err != nil {
-			return err
-		}
-		_, err = db.ExecWithTx(tx, "DROP INDEX IF EXISTS idx_secret_user_key;")
-		if err != nil {
-			return err
-		}
-
 		userIndex := fmt.Sprintf("CREATE UNIQUE INDEX IF NOT EXISTS idx_user_api_key_hash ON %s(api_key_hash);", UserTable)
-		_, err = db.ExecWithTx(tx, userIndex)
+		_, err := db.ExecWithTx(tx, userIndex)
 		if err != nil {
 			return err
 		}
